502c8f0fed88ffd67fdda5b0362229ecae7a96e7
NinaNekonoran <franzese.rozen@gmail.com>
2021-02-27 20:40:04 +0100

    change img

--- src/app/recipes.ts
+++ src/app/recipes.ts
@@ -5,7 +5,7 @@ let recipes = [{
         time_preparation : '20 min',
         time_cuisson : '2h30',
         description : 'rspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Consectetur, adipisci velit, sed quia non numquam eius modi.',
-        img : '../assets/img3.jpg',
+        img : 'https://github.com/NinaNekonoran/recette/blob/dev/src/assets/img3.jpg?raw=true',
         ingredients : [
             {name : 'Côtes de BOEUF', amount : '1kg'},
             {name : 'Gombos frais', amount : '200g'},
