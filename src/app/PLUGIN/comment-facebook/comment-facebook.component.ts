c70db1f6ac24e1a51651fe151e2a7670b8864a25
NinaNekonoran <franzese.rozen@gmail.com>
2021-03-21 10:39:06 +0100

    Change like system.

--- src/app/PLUGIN/comment-facebook/comment-facebook.component.ts
+++ src/app/PLUGIN/comment-facebook/comment-facebook.component.ts
@@ -15,7 +15,7 @@ export class CommentFacebookComponent implements OnInit, AfterViewInit{
 
   ngOnInit(): void {
     this.url = 'https://mamie-cameroun.fr/recipe/' + this.recipe.id + '/' + this.recipe.title.replaceAll(' ', '%20');
-    console.log("url : "+this.url);
+    console.log('url : '+this.url);
   }
 
   // tslint:disable-next-line:typedef
@@ -25,7 +25,7 @@ export class CommentFacebookComponent implements OnInit, AfterViewInit{
       // tslint:disable-next-line:prefer-const one-variable-per-declaration
       let js, fjs = d.getElementsByTagName(s)[0];
       js = d.createElement(s); js.id = id;
-      js.src = '//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.4';
+      js.src = '//connect.facebook.net/fr_FR/sdk.js#xfbml=1&version=v10.0&appId=191725056082943&autoLogAppEvents=1';
 
       if (d.getElementById(id)){
         // if <script id="facebook-jssdk"> exists
--- src/app/PLUGIN/plugin-like-facebook/plugin-like-facebook.component.ts
+++ src/app/PLUGIN/plugin-like-facebook/plugin-like-facebook.component.ts
@@ -1,23 +1,36 @@
-import {Component, Input, OnInit} from '@angular/core';
-import {DomSanitizer} from '@angular/platform-browser';
+import {AfterViewInit, Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-plugin-like-facebook',
   templateUrl: './plugin-like-facebook.component.html'
 })
-export class PluginLikeFacebookComponent implements OnInit {
-  private sanitizer: DomSanitizer;
+export class PluginLikeFacebookComponent implements OnInit, AfterViewInit {
   @Input()
-  url: string;
-  constructor(sanitizer: DomSanitizer) {
-    this.sanitizer = sanitizer;
-  }
+  recipe;
+
+  url;
+
   ngOnInit(): void {
+    this.url = 'https://mamie-cameroun.fr/recipe/' + this.recipe.id + '/' + this.recipe.title.replaceAll(' ', '%20');
+    console.log('url : ' + this.url);
   }
 
   // tslint:disable-next-line:typedef
-  getUrlPluginLike(){
-    return this.sanitizer.bypassSecurityTrustResourceUrl(this.url);
-  }
+  ngAfterViewInit(){
+    // tslint:disable-next-line:only-arrow-functions typedef
+    (function( d, s, id) {
+      // tslint:disable-next-line:prefer-const one-variable-per-declaration
+      let js, fjs = d.getElementsByTagName(s)[0];
+      js = d.createElement(s); js.id = id;
+      js.src = '//connect.facebook.net/fr_FR/sdk.js#xfbml=1&version=v10.0&appId=191725056082943&autoLogAppEvents=1';
 
+      if (d.getElementById(id)){
+        // if <script id="facebook-jssdk"> exists
+        delete (<any>window).FB;
+        fjs.parentNode.replaceChild(js, fjs);
+      } else {
+        fjs.parentNode.insertBefore(js, fjs);
+      }
+    }(document, 'script', 'facebook-jssdk'));
+  }
 }
